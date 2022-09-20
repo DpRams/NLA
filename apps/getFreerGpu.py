d40cd97745a75463df7102ea0b36ba4b27490330
丞哲郭 <jason1120112011201120@gmail.com>
2022-09-20 10:15:21 +0800

    暫時寫死，多加一條條件，讓筆電也可以跑 getFreerGPU.py。

--- apps/getFreerGpu.py
+++ apps/getFreerGpu.py
@@ -5,7 +5,8 @@ def getting_freer_gpu():
 
     os.system('nvidia-smi -q -d Memory | findstr Free > ./apps/tmp')
     memory_available = [int(x.split()[2]) for x in open('./apps/tmp', 'r').readlines()]
-    memory_available = [memory_available[0], memory_available[2]]
+    if len(memory_available) >= 4 : memory_available = [memory_available[0], memory_available[2]]
+    else: memory_available = [memory_available[0], memory_available[1]]
     # print(memory_available) # [10960, 11128]
     FreerGpuId = np.argmax(memory_available)
     return FreerGpuId # 0 or 1
