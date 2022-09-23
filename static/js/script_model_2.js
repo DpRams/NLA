607bafa4c3694b586a6febdd5168a1770c38c73d
丞哲郭 <jason1120112011201120@gmail.com>
2022-09-23 23:52:09 +0800

    把情境二的UI改好了 <3 。但流程應該都還要再修正，例如: 1. threshold_for_error 參數與演算法調整 2. selecting 是否與 initializing綁在一起...

--- static/js/script_model_2.js
+++ static/js/script_model_2.js
@@ -88,14 +88,99 @@ function showMatchingParameters(){
 
 
 
-function btntest(btnID){
-  let data_mask = document.getElementById("data_mask")
+function showModules(btnID){
 
-  if (btnID == "test_e"){
-    data_mask.style.color = "black";
-    data_mask.style.opacity = "1";
-    data_mask.style.pointerEvents = "";
+  // after initializing_mask is enabled, button of cramming_mask, reorganizing_mask would be enabled
+  let cramming_mask_e = document.getElementById("cramming_mask_e")
+  let cramming_mask_d = document.getElementById("cramming_mask_d")
+  let reorganizing_mask_e = document.getElementById("reorganizing_mask_e")
+  let reorganizing_mask_d = document.getElementById("reorganizing_mask_d")
+
+  let initializing_mask = document.getElementById("initializing_mask")
+  let cramming_mask = document.getElementById("cramming_mask")
+  let reorganizing_mask = document.getElementById("reorganizing_mask")
+
+  let select_initializingRule = document.getElementById("select_initializingRule")
+  let select_selectingRule = document.getElementById("select_selectingRule")
+  let select_crammingRule = document.getElementById("select_crammingRule")
+  let select_reorganizingRule = document.getElementById("select_reorganizingRule")
+
+
+  if (btnID == "initializing_mask_e"){
+  
+    cramming_mask_e.disabled = false;
+    cramming_mask_d.disabled = false;
+    reorganizing_mask_e.disabled = false;
+    reorganizing_mask_d.disabled = false;
+  
+  }else if (btnID == "initializing_mask_d"){
+
+    cramming_mask_e.disabled = true;
+    cramming_mask_d.disabled = true;
+    reorganizing_mask_e.disabled = true;
+    reorganizing_mask_d.disabled = true;
+    
   }
+
+
+  // show initializing_mask, cramming_mask, reorganizing_mask
+
+  if (btnID == "initializing_mask_e"){
+
+    initializing_mask.style.color = "black";
+    initializing_mask.style.opacity = "1";
+    initializing_mask.style.pointerEvents = "";
+
+  }else if (btnID == "initializing_mask_d"){
+
+    // prevent that button has been disabled, but the div has already enabled
+    initializing_mask.style.color = "grey";
+    initializing_mask.style.opacity = "0.65";
+    initializing_mask.style.pointerEvents = "none";
+
+    cramming_mask.style.color = "grey";
+    cramming_mask.style.opacity = "0.65";
+    cramming_mask.style.pointerEvents = "none";
+
+    reorganizing_mask.style.color = "grey";
+    reorganizing_mask.style.opacity = "0.65";
+    reorganizing_mask.style.pointerEvents = "none";
+
+    // prevent that div has already disabled, but the select value is not "Disabled"
+    select_initializingRule.selectedIndex  = (select_initializingRule.options).length-1;
+    select_selectingRule.selectedIndex  = (select_selectingRule.options).length-1;
+    select_crammingRule.selectedIndex  = (select_crammingRule.options).length-1;
+    select_reorganizingRule.selectedIndex  = (select_reorganizingRule.options).length-1;
+
+
+  }else if (btnID == "cramming_mask_e"){
+    cramming_mask.style.color = "black";
+    cramming_mask.style.opacity = "1";
+    cramming_mask.style.pointerEvents = "";
+  }else if (btnID == "cramming_mask_d"){
+    cramming_mask.style.color = "grey";
+    cramming_mask.style.opacity = "0.65";
+    cramming_mask.style.pointerEvents = "none";
+
+    // prevent that div has already disabled, but the select value is not "Disabled"
+    select_crammingRule.selectedIndex  = (select_crammingRule.options).length-1;
+  }else if (btnID == "reorganizing_mask_e"){
+    reorganizing_mask.style.color = "black";
+    reorganizing_mask.style.opacity = "1";
+    reorganizing_mask.style.pointerEvents = "";
+  }else if (btnID == "reorganizing_mask_d"){
+    reorganizing_mask.style.color = "grey";
+    reorganizing_mask.style.opacity = "0.65";
+    reorganizing_mask.style.pointerEvents = "none";
+
+    // prevent that div has already disabled, but the select value is not "Disabled"
+    select_reorganizingRule.selectedIndex  = (select_reorganizingRule.options).length-1;
+  }
+
+
+
+
+
 
 }
 
