e16e3b0f165e7e3b786026b89bf408ba500af3eb
丞哲郭 <jason1120112011201120@gmail.com>
2022-10-24 10:30:58 +0800

    container port 需固定，並非同步隨機

--- apps/ymlEditing.py
+++ apps/ymlEditing.py
@@ -6,12 +6,13 @@ from checkPort import findPortAvailable
 def findPortAvailableToYml():
 
     availablePort = findPortAvailable()
+    SERVICEPORT = 8002 # fixed
 
     ymlDict = {"stages": ["build", "test", "deploy"],
                "build": {"stage": "build", "tags": ["AILab"], "only": ["scenario_2"], "script": ["echo \"build\""]},
                "test": {"stage": "test", "tags": ["AILab"], "only": ["scenario_2"], "script": ["echo \"testing\""]},
                "deploy": {"stage": "deploy", "tags": ["AILab"],
-                          "script": ["echo \"deploy\"", "docker", "cd ASLFN", "docker build -t aslfn:latest -f rootuser.Dockerfile .", f"docker run -p {availablePort}:{availablePort} -d aslfn:latest"],
+                          "script": ["echo \"deploy\"", "docker", "cd ASLFN", "docker build -t aslfn:latest -f rootuser.Dockerfile .", f"docker run -p {availablePort}:{SERVICEPORT} -d aslfn:latest"],
                           "rules": [{"changes": ["ASLFN/docker_apps/*"]}]}}
 
     with open(".\\.gitlab-ci.yml", 'w') as file:
