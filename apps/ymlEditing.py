f229d8cd7525a50fdcc0bc000ad1f54474f1e1d6
丞哲郭 <jason1120112011201120@gmail.com>
2022-10-24 09:36:08 +0800

    ymlEditing 自動產出 yml file

--- apps/ymlEditing.py
+++ apps/ymlEditing.py
@@ -1,54 +1,13 @@
 import yaml
 import sys
 
-ymlStr = """
-stages:
-    - build
-    - test
-    - deploy
-
-build:
-  stage: build
-  tags:
-    - "AILab"
-  only:
-    - scenario_2
-  script:
-    - echo "build"
-
-test:
-  stage: test
-  tags:
-    - "AILab"
-  only:
-    - scenario_2
-  script:
-    - echo "testing"
-
-deploy:
-  stage: deploy
-  tags:
-    - "AILab"
-  script:
-    - echo "deploy"
-    - docker
-    - cd ASLFN
-    - docker build -t aslfn:latest -f rootuser.Dockerfile .
-    - docker run -p 8002:8002 -d aslfn:latest
-  rules:
-    - changes:
-      - ASLFN/docker_apps/*
-"""
-
+availablePort = 80
 ymlDict = [{"stages" : ["build", "test", "deploy"]}, \
-           {"build" : {"stage" : "build", "tags" : "AILab"}}]
-# code = yaml.load(ymlDict, Loader=yaml.Loader)
-# code["deploy"]["script"] = """
-# - echo \"deploy\"
-# - docker
-# - cd ASLFN
-# - docker build -t aslfn:latest -f rootuser.Dockerfile .
-# - docker run -p 8003:8003 -d aslfn:latest"""
-# code["build"]["stage"] = "test"
-with open("\\test.yml", 'w') as file:
+           {"build" : {"stage" : "build", "tags" : "AILab", "only" : "scenario_2", "script" : "echo \"build\""}}, \
+           {"test" : {"stage" : "test", "tags" : "AILab", "only" : "scenario_2", "script" : "echo \"testing\""}}, \
+           {"deploy" : {"stage" : "deploy", "tags" : "AILab", \
+                        "script" : ["echo \"deploy\"", "docker", "cd ASLFN", "docker build -t aslfn:latest -f rootuser.Dockerfile .", f"docker run -p {availablePort}:{availablePort} -d aslfn:latest"], \
+                        "rules" : {"changes" : "ASLFN/docker_apps/*"}}}]
+
+with open(".\\test.yml", 'w') as file:
     documents = yaml.dump(ymlDict, file)
