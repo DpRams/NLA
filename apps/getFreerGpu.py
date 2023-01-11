36561b3d2865e33a9b90306f06cbd069544b7823
丞哲郭 <jason1120112011201120@gmail.com>
2023-01-11 11:43:06 +0800

    latest

--- apps/getFreerGpu.py
+++ apps/getFreerGpu.py
@@ -4,7 +4,7 @@ import numpy as np
 def getting_freer_gpu():
 
     os.system('nvidia-smi -q -d Memory | findstr Free > ./apps/gpuMemoryTmp')
-    memory_available = [int(x.split()[2]) for x in open('./apps/tmp', 'r').readlines()]
+    memory_available = [int(x.split()[2]) for x in open('./apps/gpuMemoryTmp', 'r').readlines()]
     if len(memory_available) >= 4 : memory_available = [memory_available[0], memory_available[2]]
     else: memory_available = [memory_available[0], memory_available[1]]
     # print(memory_available) # [10960, 11128]
