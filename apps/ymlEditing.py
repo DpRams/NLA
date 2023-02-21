50896bbe3c973c10da022a9025783262d0d5007a
丞哲郭 <jason1120112011201120@gmail.com>
2023-02-21 21:36:18 +0800

    latest

--- apps/ymlEditing.py
+++ apps/ymlEditing.py
@@ -8,7 +8,7 @@ from pathlib import Path
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 
-runner_tags = ["laptop-ASUS"] # AILab
+runner_tags = ["AILab"] # AILab, laptop-ASUS
 
 def deployingModelToYml(modelId):
 
