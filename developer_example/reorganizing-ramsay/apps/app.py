cd26037a28082fab5c566784f67693db0cdca943
丞哲郭 <jason1120112011201120@gmail.com>
2023-03-05 18:04:30 +0800

    host fixed at 0.0.0.0

--- developer_example/reorganizing-ramsay/apps/app.py
+++ developer_example/reorganizing-ramsay/apps/app.py
@@ -259,7 +259,7 @@ async def pipeline_service(request: Request):
             "acceptable" : CSI.net.acceptable}
 
 if __name__ == '__main__':
-	uvicorn.run("app:app", host="127.0.0.1", port=8005, reload=True) # 若有 rewrite file 可能不行 reload=True，不然會一直重開 by 李忠大師
+	uvicorn.run("app:app", host="0.0.0.0", port=8005, reload=True) # 若有 rewrite file 可能不行 reload=True，不然會一直重開 by 李忠大師
 
 def odct_tensor2list(network):
 
