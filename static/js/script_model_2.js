9dacb3e027db3db5f369bb7e2b3428269890e009
丞哲郭 <jason1120112011201120@gmail.com>
2023-03-05 22:09:26 +0800

    latest

--- static/js/script_model_2.js
+++ static/js/script_model_2.js
@@ -111,54 +111,98 @@ function AutoFillCrammingAndReorganizingRule(rule){
   }
 }
 
-function showModules(){
+// function showModules(){
 
-  let cramming_switch = document.getElementById("cramming_switch")
-  let reorganizing_switch = document.getElementById("reorganizing_switch")
+//   let cramming_switch = document.getElementById("cramming_switch")
+//   let reorganizing_switch = document.getElementById("reorganizing_switch")
 
-  let cramming_mask = document.getElementById("cramming_mask")
-  let reorganizing_mask = document.getElementById("reorganizing_mask")
+//   let cramming_mask = document.getElementById("cramming_mask")
+//   let reorganizing_mask = document.getElementById("reorganizing_mask")
 
-  let select_crammingRule = document.getElementById("select_crammingRule")
-  let select_reorganizingRule = document.getElementById("select_reorganizingRule")
+//   let select_crammingRule = document.getElementById("select_crammingRule")
+//   let select_reorganizingRule = document.getElementById("select_reorganizingRule")
 
-  let reorganizingParamaters = document.getElementById("reorganizingParamaters")
+//   let reorganizingParamaters = document.getElementById("reorganizingParamaters")
   
-  if (cramming_switch.checked == true){
-    cramming_mask.style.color = "black";
-    cramming_mask.style.opacity = "1";
-    cramming_mask.style.pointerEvents = "";
-    AutoFillCrammingAndReorganizingRule('cramming');
+//   if (cramming_switch.checked == true){
+//     cramming_mask.style.color = "black";
+//     cramming_mask.style.opacity = "1";
+//     cramming_mask.style.pointerEvents = "";
+//     AutoFillCrammingAndReorganizingRule('cramming');
 
-  }else if (cramming_switch.checked == false){
-    cramming_mask.style.color = "grey";
-    cramming_mask.style.opacity = "0.65";
-    cramming_mask.style.pointerEvents = "none";
+//   }else if (cramming_switch.checked == false){
+//     cramming_mask.style.color = "grey";
+//     cramming_mask.style.opacity = "0.65";
+//     cramming_mask.style.pointerEvents = "none";
 
-    // prevent that div has already disabled, but the select value is not "Disabled"
-    select_crammingRule.selectedIndex  = (select_crammingRule.options).length-1;
-  }
+//     // prevent that div has already disabled, but the select value is not "Disabled"
+//     select_crammingRule.selectedIndex  = (select_crammingRule.options).length-1;
+//   }
   
-  if (reorganizing_switch.checked == true){
-    reorganizing_mask.style.color = "black";
-    reorganizing_mask.style.opacity = "1";
-    reorganizing_mask.style.pointerEvents = "";
-    AutoFillCrammingAndReorganizingRule('reorganizing');
+//   if (reorganizing_switch.checked == true){
+//     reorganizing_mask.style.color = "black";
+//     reorganizing_mask.style.opacity = "1";
+//     reorganizing_mask.style.pointerEvents = "";
+//     AutoFillCrammingAndReorganizingRule('reorganizing');
 
-    reorganizingParamaters.style.display = "";
+//     reorganizingParamaters.style.display = "";
     
-  }else if (reorganizing_switch.checked == false){
-    reorganizing_mask.style.color = "grey";
-    reorganizing_mask.style.opacity = "0.65";
-    reorganizing_mask.style.pointerEvents = "none";
+//   }else if (reorganizing_switch.checked == false){
+//     reorganizing_mask.style.color = "grey";
+//     reorganizing_mask.style.opacity = "0.65";
+//     reorganizing_mask.style.pointerEvents = "none";
 
-    // prevent that div has already disabled, but the select value is not "Disabled"
-    console.log((select_reorganizingRule.options).length-1);
-    select_reorganizingRule.selectedIndex  = (select_reorganizingRule.options).length-1;
+//     // prevent that div has already disabled, but the select value is not "Disabled"
+//     console.log((select_reorganizingRule.options).length-1);
+//     select_reorganizingRule.selectedIndex  = (select_reorganizingRule.options).length-1;
 
-    reorganizingParamaters.style.display = "none";
+//     reorganizingParamaters.style.display = "none";
+//   }
+
+// }
+
+
+function showModules() {
+  // Get all the necessary elements from the DOM
+  const crammingSwitch = document.getElementById("cramming_switch");
+  const reorganizingSwitch = document.getElementById("reorganizing_switch");
+  const crammingMask = document.getElementById("cramming_mask");
+  const reorganizingMask = document.getElementById("reorganizing_mask");
+  const selectCrammingRule = document.getElementById("select_crammingRule");
+  const selectReorganizingRule = document.getElementById("select_reorganizingRule");
+  const reorganizingParameters = document.getElementById("reorganizingParamaters");
+  
+  // Check if the cramming switch is checked
+  if (crammingSwitch.checked) {
+    // If it is, set the appropriate styles and call the AutoFillCrammingAndReorganizingRule function with "cramming" as the parameter
+    crammingMask.style.color = "black";
+    crammingMask.style.opacity = "1";
+    crammingMask.style.pointerEvents = "";
+    AutoFillCrammingAndReorganizingRule('cramming');
+  } else {
+    // If it's not, set the appropriate styles and select the last option in the select element
+    crammingMask.style.color = "grey";
+    crammingMask.style.opacity = "0.65";
+    crammingMask.style.pointerEvents = "none";
+    selectCrammingRule.selectedIndex = selectCrammingRule.options.length - 1;
   }
-
+  
+  // Check if the reorganizing switch is checked
+  if (reorganizingSwitch.checked) {
+    // If it is, set the appropriate styles, call the AutoFillCrammingAndReorganizingRule function with "reorganizing" as the parameter, and show the reorganizing parameters element
+    reorganizingMask.style.color = "black";
+    reorganizingMask.style.opacity = "1";
+    reorganizingMask.style.pointerEvents = "";
+    AutoFillCrammingAndReorganizingRule('reorganizing');
+    reorganizingParameters.style.display = "";
+  } else {
+    // If it's not, set the appropriate styles, select the last option in the select element, and hide the reorganizing parameters element
+    reorganizingMask.style.color = "grey";
+    reorganizingMask.style.opacity = "0.65";
+    reorganizingMask.style.pointerEvents = "none";
+    selectReorganizingRule.selectedIndex = selectReorganizingRule.options.length - 1;
+    reorganizingParameters.style.display = "none";
+  }
 }
 
 function commonLearningGoal(value){
