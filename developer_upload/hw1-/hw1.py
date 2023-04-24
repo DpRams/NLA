25b1fa1e413bbe54c7de9cb67ee5c11516b876c3
丞哲郭 <jason1120112011201120@gmail.com>
2023-04-24 12:39:38 +0800

    latest

--- developer_upload/hw1-/hw1.py
+++ developer_upload/hw1-/hw1.py
@@ -6,16 +6,16 @@ import requests
 
 # Step 1. 填寫下面的變數，變數請從右方註解中挑選
 
-dataDirectory = "hospice"              # "solar", "hospice"
-hiddenNode = "18"                 # any integer
-weightInitialization = "xavierUniform"       # "xavierNormal", "xavierUniform", "kaimingNormal", "kaimingUniform"
-activationFunction = "tanh"         # "ReLU", "tanh"
-epoch = "26"                      # any integer
+dataDirectory = ""              # "solar", "hospice"
+hiddenNode = ""                 # any integer
+weightInitialization = ""       # "xavierNormal", "xavierUniform", "kaimingNormal", "kaimingUniform"
+activationFunction = ""         # "ReLU", "tanh"
+epoch = ""                      # any integer
 lossFunction = "MSE"  
-regularizationTerm = "0"         # "0", "0.001", "0.0001"
-optimizer = "gradientDescent"                  # "Adam", "gradientDescent", "Momentum"
-learningRateDecayScheduler = "None" # "None", "Cosine"
-studentId = "110356021"                  # Your student ID, e.g., "110356021"
+regularizationTerm = ""         # "0", "0.001", "0.0001"
+optimizer = ""                  # "Adam", "gradientDescent", "Momentum"
+learningRateDecayScheduler = "" # "None", "Cosine"
+studentId = ""                  # Your student ID, e.g., "110356021"
 
 # Step 2. 壓縮 hw1 資料夾，並編輯名稱為 "hw1-{studentId}.zip", e.g., hw1-110356021.zip 
 # Step 3. 於 http://140.119.19.87/pipeline/develop 上傳壓縮檔即可
